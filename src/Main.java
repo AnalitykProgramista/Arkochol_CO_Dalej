f153c4bd58ab6d82ee3b31bb41911d5ba0837309
student <pawel_lenartowicz_it@interia.pl>
2023-02-15 11:08:34 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Dzień dobry!");
-        System.out.println("Ma na imie Pietrek");
+    public static void main(String[] args) throws IOException {
+
+        //sekcja wprowadzenia danych
+        Double zmiennaP, zmiennaA, zmiennaK, zmiennaW;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Programik oblicza, czy Jan Kowalki jest ma promile!");
+        System.out.println("Podaj ilość wypitego czystego alkoholu w gramach.");
+        zmiennaA = Double.parseDouble(br.readLine());
+        System.out.println("Podaj -> stały współczynnik 0,7 dla mężczyzn i 0,6 dla kobiet.");
+        zmiennaK = Double.parseDouble(br.readLine());
+        System.out.println("Podaj masę ciała w kg");
+        zmiennaW = Double.parseDouble(br.readLine());
+
+        zmiennaP = (zmiennaA / (zmiennaK * zmiennaW));
+        System.out.print("Wynik: " + zmiennaP + "\n");
+
+        //sekcja promili
+        double Inf1 = 0.2;
+        double nazwisko, imie;
+        if (Inf1 < zmiennaP){
+            System.out.println("Kierowca jest do .... i nie ma prawa prowadzic pojazdu na terenie Polski");
+            System.out.println("Kierowca może spbie tylko pomarzyć wirtualnie \n");
+        }
+        if (Inf1 > zmiennaP){
+            System.out.println("Kierowca musi uważać ... i basta");
+        }
+
     }
 }
